9adcc6bc1c954862d862aecebc0f367c90d3d159
John Need <john.need@gmail>
2017-08-05 16:32:29 -0400

    bug fixes

--- src/app/app.js
+++ src/app/app.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 	var mobileMenu = $('#content');
 	$('#hamburger').click(function () {
 		$(this).toggleClass('open');
+		$(document.body).toggleClass('is-menu-open');
 		mobileMenu.toggleClass('is-menu-open');
 	});
 });
