6c7e39851b559570975be1ddf007d0ad5367dded
John Need <john.need@gmail>
2017-09-27 17:05:51 -0400

    changed npm start

--- init.js
+++ init.js
@@ -1,33 +1,29 @@
 /* global module:true */
-/* eslint no-console: 0 , no-magic-numbers: 0,  strict: [2, "functional"] */
-
-"use strict";
+/* eslint no-console: 0 , no-magic-numbers: 0,  strict: 2 */
 
 
-const server = require('./server');
+const server = require("./dev_server");
 const build = require("./build");
-const port = "8081";
-const opn = require('opn');
-const watch = require('node-watch');
-const WebSocket = require('ws');
+const port = "8080";
+const opn = require("opn");
+const watch = require("node-watch");
+const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port:8082 });
+const wss = new WebSocket.Server({port: 8082});
 
-let sendReloadMessage;
- 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
-  ws.send('something');
+wss.on("connection", (ws) => {
+    ws.on("message", (message) => {
+        console.log("received: %s", message);
+    });
+    ws.send("something");
 });
 
 wss.broadcast = function broadcast(data) {
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data);
-    }
-  });
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+        }
+    });
 };
 
 // const launcher = require('simple-autoreload-server');
@@ -36,10 +32,10 @@ async function start() {
     await server(port);
     opn(`http://localhost:${port}`);
 
-    watch('src', { recursive: true }, async function (evt, name) {
+    watch("src", {recursive: true}, async (evt, name) => {
         console.log(`${name} changed : re-buliding`);
         await build();
-     wss.broadcast('reload');
+        wss.broadcast("reload");
     });
 }
 start();
--- server.js
+++ server.js
@@ -1,54 +1,40 @@
-/* global module:true */
-/* eslint no-console: 0 , no-magic-numbers: 0,  strict: [2, "functional"] */
-
-"use strict";
-
-const defaultPort = 8080;
 const http = require("http");
+const port = 8080;
 const url = require("url");
 const path = require("path");
 const fs = require("fs");
-const mime = require('mime');
+const mime = require("mime");
 const distPath = "dist";
-const port = process.argv[2] || defaultPort;
-const refreshScript = '<script>const ws=new WebSocket("ws://localhost:8082");ws.onopen=function(){ws.send("foo")},ws.onmessage=function(o){"reload"===o.data&&location.reload()};</script>';
-module.exports = async function start(port) {
-    port = port || defaultPort;
-    http.createServer((request, response) => {
 
-        const uri = `${distPath}/${url.parse(request.url).pathname}`;
-        let filename = path.join(process.cwd(), uri);
+http.createServer((request, response) => {
 
-        fs.exists(filename, (exists) => {
-            if (!exists) {
-                response.writeHead(404, { "Content-Type": "text/plain" });
-                response.write("404 Not Found\n");
+    const uri = `${distPath}/${url.parse(request.url).pathname}`;
+    let filename = path.join(process.cwd(), uri);
+
+    fs.exists(filename, (exists) => {
+        if (!exists) {
+            response.writeHead(404, {"Content-Type": "text/plain"});
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        }
+
+        if (fs.statSync(filename).isDirectory()) {
+            filename += "/index.html";
+        }
+
+        fs.readFile(filename, "binary", (err, file) => {
+            if (err) {
+                response.writeHead(500, {"Content-Type": "text/plain"});
+                response.write(`${err}\n`);
                 response.end();
                 return;
             }
 
-            if (fs.statSync(filename).isDirectory()) {
-                filename += "/index.html";
-            }
-
-            fs.readFile(filename, "binary", (err, file) => {
-                if (err) {
-                    response.writeHead(500, { "Content-Type": "text/plain" });
-                    response.write(`${err}\n`);
-                    response.end();
-                    return;
-                }
-                //insert script for autorefreshing page
-                if (filename.endsWith(".html")) {
-                    file = file.replace("</body>", `${refreshScript}</body>`);
-                }
-                response.setHeader('content-type', mime.lookup(filename));
-                response.writeHead(200);
-                response.write(file, "binary");
-                response.end();
-            });
+            response.setHeader("content-type", mime.lookup(filename));
+            response.writeHead(200);
+            response.write(file, "binary");
+            response.end();
         });
-    }).listen(parseInt(port, 10));
-
-    console.log(`Static file server running at\n  => http://localhost:${port} /\nCTRL + C to shutdown`);
-};
+    });
+}).listen(parseInt(port, 10));
